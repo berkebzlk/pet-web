71fd579423a327c3b644ff359ed08ba0c163494c
berkebzlk <berkebozlak10@hotmail.com>
2025-12-29 03:44:19 +0300

    fix(ui): fix scroll positioning and layout issues

--- src/shared/components/layout/RootLayout.tsx
+++ src/shared/components/layout/RootLayout.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { useMatchNotifications } from '@/modules/match/hooks/useMatchNotifications';
+import { ScrollToTop } from '../utils/ScrollToTop';
 
 export const RootLayout = () => {
     useMatchNotifications();
 
-    return <Outlet />;
+    return (
+        <>
+            <ScrollToTop />
+            <Outlet />
+        </>
+    );
 };
